debf17e7e2884522d4579801a191da41363551fd
SheshanthB <91946409+SheshanthB@users.noreply.github.com>
2024-01-30 18:44:30 +0530

    second

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="flex m-10">
+      <img src="/favicon.ico" alt="logo" />
+      <h1 class="text-2xl px-4">Nuxt Task</h1>
+    </div>
+    <slot />
+  </div>
+</template>
